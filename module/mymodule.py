b6ca8b0fdb8a2126c61464d7c3512d172371f6a6
Aruni Mishra <arunimishramsit@gmail.com>
2021-02-03 23:06:58 +0530

    __name__

--- module/mymodule.py
+++ module/mymodule.py
@@ -1,2 +1,8 @@
 def my_func():
 	print("this is a my_func inside mymodule")
+
+
+if __name__ == '__main__':
+	print("ran directly- mymodule")
+else:
+	print("ran indirectly- mymodule")
--- module/myprogram.py
+++ module/myprogram.py
@@ -1,3 +1,6 @@
 from mymodule import my_func
 
 my_func()
+
+if __name__ == '__main__':
+	print("ran directly-myprogram")
