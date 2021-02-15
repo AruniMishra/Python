b98a29c2783c239003323725b5b7250aef2a2ec1
Aruni Mishra <arunimishramsit@gmail.com>
2021-02-15 22:29:55 +0530

    email : send

---
+++ Emailing-with-Python/email_send.py
@@ -0,0 +1,48 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+import smtplib
+import getpass
+
+
+# %%
+smtp_object = smtplib.SMTP('smtp.gmail.com', 587)
+
+
+# %%
+smtp_object.ehlo()
+
+
+# %%
+smtp_object.starttls()
+
+
+
+# %%
+#Do not use
+#password = input('what is your password')
+
+
+# %%
+email = getpass.getpass("Enter your email: ")
+password = getpass.getpass("Enter your password: ")
+smtp_object.login(email,password)
+
+
+# %%
+from_address = getpass.getpass("Enter your email: ")
+to_address = getpass.getpass("Enter the email of the recipient: ")
+subject = input("Enter the subject line: ")
+message = input("Type out the message you want to send: ")
+msg = "Subject: " + subject + '\n' + message
+smtp_object.sendmail(from_address,to_address,msg)
+
+
+# %%
+#Ref: https://support.google.com/accounts/answer/185833?hl=en/
+
+
+# %%
+
+
+
