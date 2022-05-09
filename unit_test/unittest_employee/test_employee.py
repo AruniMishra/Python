1b6dc951eae335b415d2b270a1787b480f68e7a7
Aruni Mishra <arunimishramsit@gmail.com>
2022-05-09 23:16:06 +0200

    unittest

---
+++ unit_test/unittest_employee/test_employee.py
@@ -0,0 +1,29 @@
+import unittest
+from employee import Employee
+
+class TestEmployee(unittest.TestCase):
+
+    '''
+    setUpClass and tearDownClass are run once for the whole class; setUp and tearDown are run before and after each test method
+    '''
+
+    @classmethod
+    def setUpClass(cls):
+        print('setupClass')
+
+    @classmethod
+    def tearDownClass(cls):
+        print('teardownClass')
+
+    def setUp(self):
+        print('setUp')
+        self.emp_1 = Employee('Corey', 'Schafer', 50000)
+        self.emp_2 = Employee('Sue', 'Smith', 60000)
+
+    def tearDown(self):
+        print('tearDown\n')
+
+    def test_apply_raise(self):
+        print('test_apply_raise')
+        self.emp_1.apply_raise()
+        self.assertEqual(self.emp_1.pay, 100000)
