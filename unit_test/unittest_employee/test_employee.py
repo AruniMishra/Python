e1038bcd0c9065caef3b9d2dc7a4fe6ab5f248b1
Aruni Mishra <arunimishramsit@gmail.com>
2022-05-10 21:48:48 +0200

    unittest

--- unit_test/unittest_employee/test_employee.py
+++ unit_test/unittest_employee/test_employee.py
@@ -1,5 +1,7 @@
 import unittest
+from unittest.mock import patch
 from employee import Employee
+
 
 class TestEmployee(unittest.TestCase):
 
@@ -27,6 +29,22 @@ class TestEmployee(unittest.TestCase):
         print('test_apply_raise')
         self.emp_1.apply_raise()
         self.assertEqual(self.emp_1.pay, 100000)
+
+    def test_monthly_schedule(self):
+        with patch('employee.requests.get') as mocked_get:
+            mocked_get.return_value.ok = True
+            mocked_get.return_value.text = 'Success'
+
+            schedule = self.emp_1.monthly_schedule('May')
+            mocked_get.assert_called_with('http://company.com/Schafer/May')
+            self.assertEqual(schedule, 'Success')
+
+            mocked_get.return_value.ok = False
+
+            schedule = self.emp_2.monthly_schedule('June')
+            mocked_get.assert_called_with('http://company.com/Smith/June')
+            self.assertEqual(schedule, 'Bad Response!')
+
 
 if __name__ == '__main__':
     unittest.main()
